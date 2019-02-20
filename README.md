375b455455609e2ce51fbc384b7a52db0513095d
Stephen R. van den Berg <srb@cuci.nl>
2019-02-20 01:24:06 +0100

    Let the framework handle diagnostic messages.

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 <h1>Outperform</h1>
 
-Outperform is a form generator written in plain Javascript.
+Outperform is a form generator written in plain Javascript with zero
+dependencies.
+It supports and augments browser native form validation and offers automatic
+persistence of half-filled forms across page reloads.
 
 ## Requirements
 
 It runs inside any webbrowser environment (starting at IE11 and up).
+
+## Features
+
+- Zero dependencies.
+- Less filling (unminified but gzipped: <2KB).
+- Native browser form validation support.
+- Custom validation seemlessly integrated.
+- Compact form description format.
+- Automatic and consistent persistence of forms across page reloads in
+  all browsers.
+- Convenience functions to easily retrieve and (pre)set all form data using
+  javascript objects (ideal for custom processing of form submissions).
 
 ## Basic usage
 
@@ -13,13 +28,60 @@ It runs inside any webbrowser environment (starting at IE11 and up).
    [ "houseno", "number", "This is the description", {min:1, max:999}],
    [ "foo", "text", "This is the description"],
    [ "foo2", "password", "This is the description"],
+   [ "bar", "select", "This is the description",
+     {list:
+      ["one",
+       ["two", {class:"bright"}],
+       ["three", "Three choices"],
+       "four",
+       ["five", "Five", {style:"display:none;"}]
+      ]
+     }
+   ],
+   [ "bar2", "checkbox", "This is the description",
+     {list:
+      ["one",
+      ["two", {class:"bright", value:"2"}],
+      ["Three choices"],
+      "four",
+      ["Five", {style:"display:none;", value:"5"}]]
+     }
+   ],
  ];
- var form = forms.create("form", {id:"hello"}, fields);
+ var form = Outperform.create("form", {id:"hello"}, fields);
  document.body.appendChild(form);
+ console.log(JSON.stringify(Outperform.getfields(form)));
 ```
 
 ## Reference documentation
 
 ### API
+
+Specified parameters:
+- `form`<br />
+  A reference to the current form node.
+- `fielddescription`<br />
+  The description of an input field.
+- `fields`<br />
+  An array of `fielddescription` fields.
+
+Exposed API-list:
+- `Outperform.create(formtag, attributes, fields)`<br />
+  Creates an HTML element using the tag in the string `formtag`
+  (usually `"form"`, unless you deviate from HTML5) adorned by the
+  attributes listed in the `attributes` object.  The form is filled
+  using the fielddescriptions in the `fields` array.  It returns
+  a reference to the form node which should normally be inserted into
+  the DOM.
+- `Outperform.clearpersist(form)`<br />
+  Clears the persistent storage used to store half-filled form data.
+  Intended to be used right after submitting the form successfully.
+- `Outperform.getvalues(form)`<br />
+  Returns an object containing all entered values in the form.
+- `Outperform.setvalues(form, values)`<br />
+  (Pre)sets all values in the form using the object in `values`.
+- `Outperform.field(form, fielddescription)`<br />
+   Return a single node that is equivalent to the provided `fielddescription`
+   (used internally by `Outperform.create()`).
 
 ## References
--- outperform.js
+++ outperform.js
@@ -5,9 +5,9 @@
 
 !function(W, D, O)
 { "use strict";
-  var diva = newel("div"), labelsel = ".label", errsel = ".error",
+  var diva = newel("div"), labelsel = ".label",
    template = txt2node("<label><span class=\"label\"></span>"
-    + "<input/><span class=\"error\"></span></label>"),
+    + "<input /></label>"),
    inputs = "input,select,textarea";
   function isstring(s)
   { return O.prototype.toString.call(s) === "[object String]"; }
@@ -15,6 +15,7 @@
   function isa(s) { return Array.isArray(s); }
   function sattr(n, k, v) { n.setAttribute(k, v); }
   function addattr(el, a) { for (var i in a) sattr(el, i, a[i]); }
+  function replelm(n, o) { return o.parentNode.replaceChild(n, o); }
   function newel(n, a) { addattr(n = D.createElement(n), a); return n; }
 
   function txt2node(t)
@@ -38,85 +39,125 @@
   var g =
   { create: function(form, attr, fields)
       { form = newel(form, attr);
-        for (attr = 0; attr < fields.length; attr++)
-          form.appendChild(g.field(form, fields[attr]));
-        form.addEventListener("change", storeform);
-        try
-        { if (fields = sessionStorage.getItem("form#" + form.id))
-            g.setvalues(form, JSON.parse(fields), 1);
-        } catch(e) {}
-        return form;
+	for (attr = 0; attr < fields.length; attr++)
+	  form.appendChild(g.field(form, fields[attr]));
+	form.addEventListener("change", storeform);
+	try
+	{ if (fields = sessionStorage.getItem("form#" + form.id))
+	    g.setvalues(form, JSON.parse(fields), 1);
+	} catch(e) {}
+	return form;
+      },
+    clearpersist: function(form)
+      { try { sessionStorage.removeItem("form#" + form.id); } catch(e) {}
       },
     getvalues: function(form, persist)
       { var i, el, nm, t, els = form.elements, res = {};
-        for (i = 0; i < els.length;)
-        { switch((el = els[i++]).tagName)
-          { default: continue;
-            case "INPUT": case "SELECT": case "TEXTAREA":;
-          }
-          nm = el.name;
-          if (persist && !el.opfpersist)
-            continue;
-          switch(el.type)
-          { case "checkbox":
-            case "radio":
-              if (el.checked)
-                if ((t = res[nm]) === undefined)
-                  res[nm] = el.value;
-                else
-                { if (!isa(t))
-                    res[nm] = [t];
-                  res[nm].push(el.value);
-                }
-              break;
-            default: res[nm] = el.value;
-          }
-        }
-        return res;
+	for (i = 0; i < els.length;)
+	{ switch((el = els[i++]).tagName)
+	  { default: continue;
+	    case "INPUT": case "SELECT": case "TEXTAREA":;
+	  }
+	  nm = el.name;
+	  if (persist && !el.opfpersist)
+	    continue;
+	  switch(el.type)
+	  { case "checkbox":
+	    case "radio":
+	      if (el.checked)
+		if ((t = res[nm]) === undefined)
+		  res[nm] = el.value;
+		else
+		{ if (!isa(t))
+		    res[nm] = [t];
+		  res[nm].push(el.value);
+		}
+	      break;
+	    default: res[nm] = el.value;
+	  }
+	}
+	return res;
       },
     setvalues: function(form, vals, persist)
       { var i, el, nm, t, els = form.elements;
-        for (i = 0; i < els.length;)
-        { switch((el = els[i++]).tagName)
-          { default: continue;
-            case "INPUT": case "SELECT": case "TEXTAREA":;
-          }
-          nm = el.name;
-          if (persist && !el.opfpersist)
-            continue;
-          switch(el.type)
-          { case "checkbox":
-            case "radio":
-              el.checked = isa(t = vals[nm]) && t.indexOf(el.value) >= 0
-               || t === el.value;
-              break;
-            default: el.value = vals[nm];
-          }
-        }
+	for (i = 0; i < els.length;)
+	{ switch((el = els[i++]).tagName)
+	  { default: continue;
+	    case "INPUT": case "SELECT": case "TEXTAREA":;
+	  }
+	  nm = el.name;
+	  if (persist && !el.opfpersist)
+	    continue;
+	  switch(el.type)
+	  { case "checkbox":
+	    case "radio":
+	      el.checked = isa(t = vals[nm]) && t.indexOf(el.value) >= 0
+	       || t === el.value;
+	      break;
+	    default: el.value = vals[nm];
+	  }
+	}
       },
     field: function(form, fld)
-      { var opt = fld[3] = fld[3] || {}, k, i, t, iel,
-         v = { name:fld[0], type:fld[1] };
-        function reportvalidity(el)
-        { el = el.currentTarget;
-          if (opt.validate)
-            el.setCustomValidity(opt.validate(form.elements) || "");
-          el.reportValidity();
-        }
-        O.assign(v, opt);
-        k = (opt.template = txt2node(opt.template || template)).cloneNode(1);
-        k.querySelector(opt.labelsel || labelsel).textContent = fld[2];
-        delete v.errsel; delete v.labelsel;
-        delete v.validate; delete v.persist; delete v.template;
-        for (t = k.querySelectorAll(inputs), i = 0; i < t.length; i++)
-        { addattr(iel = t[i], v);
-          iel.addEventListener("change", reportvalidity);
-          iel.addEventListener("input", reportvalidity);
-          iel.opfpersist = opt.persist !== undefined ? opt.persist :
-           v.type == "password" ? 0 : 1;
-          iel.opferror = k.querySelector(opt.errsel || errsel);
-        }
-        return k;
+      { var opt = fld[3] = fld[3] || {}, k, i, j, m, t, r, li, le, iel,
+	 v = { name:fld[0], type:fld[1] };
+	function reportvalidity(el)
+	{ el = el.currentTarget;
+	  if (opt.validate)
+	    el.setCustomValidity(opt.validate(form.elements) || "");
+	  el.reportValidity();
+	}
+	O.assign(v, opt);
+	k = (opt.template = txt2node(opt.template || template)).cloneNode(1);
+	k.querySelector(opt.labelsel || labelsel).textContent = fld[2];
+	delete v.labelsel;delete v.list;
+	delete v.validate; delete v.persist; delete v.template;
+	switch (v.type)
+	{ case "checkbox": case "radio":
+	    if (v.value === undefined)
+	      v.value = fld[2];
+	}
+	for (t = k.querySelectorAll(inputs), i = 0; i < t.length; i++)
+	{ iel = t[i];
+	  switch (v.type)
+	  { case "checkbox": case "radio":
+	      if (li = opt.list)
+	      { le = iel.parentNode;
+		for (j = 0; j < li.length;)
+		{ if (!isa(m = li[j++]))
+		    m = [m];
+		  if (m.length < 3)
+		    (m = m.slice()).unshift(fld[0], fld[1]);
+		  le.insertBefore(g.field(form, m), iel);
+		}
+		le.removeChild(iel);
+		continue;
+	      }
+	      break;
+	    case "select": case "textarea":
+	      replelm(m = newel(v.type), iel); iel = m;
+	  }
+	  addattr(iel, v);
+	  iel.addEventListener("change", reportvalidity);
+	  iel.addEventListener("input", reportvalidity);
+	  iel.opfpersist = opt.persist !== undefined ? opt.persist :
+	   v.type == "password" ? 0 : 1;
+	  if (v.type == "select")
+	  { li = opt.list || [];
+	    for (j = 0; j < li.length;)
+	    { if (!isa(r = li[j++]))
+		r = [r];
+	      if (isstring(r[1]))
+		(m = r[2] || {}).value = r[0];
+	      else
+		m = r[1] || {};
+	      le = newel("option", m);
+	      le.textContent = isstring(r[1]) ? r[1] : r[0];
+	      iel.appendChild(le);
+	    }
+	  }
+	}
+	return k;
       }
   };
 
